80633bb208e95396a43f509503cae7058445ccc6
lawtlee <lawsac2947@gmail.com>
2023-10-30 14:39:39 -0700

    Updated the file to start working
    I don't know why the chunking doesn't analyze the total 500 chunks but I'm not sure.

--- batch/lawrence_batch_test.R
+++ batch/lawrence_batch_test.R
@@ -3,28 +3,32 @@ library(elevatr)
 library(sf)
 library(readr)
 library(dplyr)
-dat = read_csv('~/Personal/DataSquad/helicopter-data/test.csv') # this file is also located in the server
 
+# ORIGINAL CODE
+# dat = read_csv('~/Personal/DataSquad/R-Code-Refactorization/test.csv') # this file is also located in the server
+# crs_dd <- 4326
+# sf_dat <- sf::st_as_sf(dat, coords = c("longitude", "latitude"), crs = crs_dd)
+# df_elev_epqs <- get_elev_point(sf_dat, prj = crs_dd, src = "aws", z=10)
+
+
+# ALTERED CODE
+filePath = "~/Personal/DataSquad/helicopter-data/truncate_tracts_5000.csv"
 crs_dd <- 4326
-
-sf_dat <- sf::st_as_sf(dat, coords = c("longitude", "latitude"), crs = crs_dd)
-
 data <- data.frame()
-
 data_env <- new.env()
 
+# Callback Function, its gonna process the chunked data
 f <- function(x, pos) {
-  #print(x)
-  df_elev_aws <- get_elev_point(sf_dat, prj = crs_dd, src = "aws", z=10)
+  #test_dat <- sf::st_as_sf(x, coords = c("longitude", "latitude"), crs = crs_dd)
 
-  #print(df_elev_aws)
-
+  df_elev_aws <- get_elev_point(test_dat, prj = crs_dd, src = "aws", z=10)
   data_env$data <- rbind(data, df_elev_aws)
+
+  #df_elev_epqs <- get_elev_point(x, prj = crs_dd, src = "epqs")
+  #data_env$data <- rbind(data, df_elev_epqs)
 }
 
-read_csv_chunked('~/Personal/DataSquad/helicopter-data/test.csv', DataFrameCallback$new(f), chunk_size = 500)
+read_csv_chunked(filePath, DataFrameCallback$new(f), chunk_size = 500)
+# read_delim_chunked('~/Personal/DataSquad/helicopter-data/test.csv', delim = ',', skip = 0, DataFrameCallback$new(f),)
 
 data <- data_env$data
-
-
-#read_delim_chunked('~/Personal/DataSquad/helicopter-data/test.csv', delim = ',', skip = 0, DataFrameCallback$new(f),)
