31abe8edc90c42f0f51cf98b921d38782a944869
Lawrence Lee <104863284+lawtlee@users.noreply.github.com>
2023-10-30 13:57:40 -0700

    Create aws_optimization.R

---
+++ aws_optimization.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(elevatr)
+library(sf)
+library(readr)
+library(dplyr)
+dat = read_csv('~/Personal/DataSquad/R-Code-Refactorization/test.csv') # this file is also located in the server
+
+crs_dd <- 4326
+
+sf_dat <- sf::st_as_sf(dat, coords = c("longitude", "latitude"), crs = crs_dd)
+
+# The switch to AWS is to efficiently process it, EPQS is very accurate but doesn't work very well on >500 data points. z=10 is the sweet spot on how
+# accurate you want the AWS server to be
+df_elev_epqs <- get_elev_point(sf_dat, prj = crs_dd, src = "aws", z=10) 
+
