67fb47908533d6716993cb905181263419604d9f
lawtlee <lawsac2947@gmail.com>
2023-10-30 14:16:40 -0700

    Updated lawrence_batch_test.R
    Can work with read_csv_chunked but not with
    read_delim_chunked.

--- batch/lawrence_batch_test.R
+++ batch/lawrence_batch_test.R
@@ -3,7 +3,7 @@ library(elevatr)
 library(sf)
 library(readr)
 library(dplyr)
-dat = read_csv('~/Personal/DataSquad/R-Code-Refactorization/test.csv') # this file is also located in the server
+dat = read_csv('~/Personal/DataSquad/helicopter-data/test.csv') # this file is also located in the server
 
 crs_dd <- 4326
 
@@ -11,13 +11,20 @@ sf_dat <- sf::st_as_sf(dat, coords = c("longitude", "latitude"), crs = crs_dd)
 
 data <- data.frame()
 
+data_env <- new.env()
+
 f <- function(x, pos) {
   #print(x)
-  df_elev_epqs <- get_elev_point(sf_dat, prj = crs_dd, src = "epqs")
+  df_elev_aws <- get_elev_point(sf_dat, prj = crs_dd, src = "aws", z=10)
 
-  rbind(data, df_elev_epqs)
+  #print(df_elev_aws)
+
+  data_env$data <- rbind(data, df_elev_aws)
 }
 
-#read_csv_chunked('~/Personal/DataSquad/R-Code-Refactorization/test.csv', DataFrameCallback$new(f), chunk_size = 5)
+read_csv_chunked('~/Personal/DataSquad/helicopter-data/test.csv', DataFrameCallback$new(f), chunk_size = 500)
 
-read_delim_chunked('~/Personal/DataSquad/R-Code-Refactorization/test.csv', delim = ',', skip = 0, DataFrameCallback$new(f),)
+data <- data_env$data
+
+
+#read_delim_chunked('~/Personal/DataSquad/helicopter-data/test.csv', delim = ',', skip = 0, DataFrameCallback$new(f),)
