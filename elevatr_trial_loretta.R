28fc16d41409f4fea9674b98a2ea7f61fef17851
Loretta Hu <67490422+lorhzy09@users.noreply.github.com>
2023-10-30 17:07:08 -0700

    update code

--- elevatr_trial_loretta.R
+++ elevatr_trial_loretta.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(elevatr)
 library(sf)
-dat = read_csv('/Users/hzy/Desktop/23fall/DSC/R code refactorization/truncate_tracts_updated.csv') # this file is also located in the server 
+dat = read_csv('/Users/hzy/Desktop/23fall/DSC/R code refactorization/truncate_tracts_updated.csv') # this file is also located in the server
 head(dat)
 
 colnames(dat)[which(names(dat) == "geometry")] <-"latitude"
@@ -12,6 +12,6 @@ crs_dd <- 4326
 sf_dat <- sf::st_as_sf(dat, coords = c("longitude", "latitude"), crs = crs_dd)
 
 df_elev_epqs <- get_elev_point(sf_dat, prj = crs_dd, src = "aws", z=10)
+summary(df_elev_epqs)
 
-
-read_delim_chunked(dat)
+read_delim_chunked(df_elev_epqs)
