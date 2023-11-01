d0641253e642034281092a90e411ac23a51713dc
lawtlee <lawsac2947@gmail.com>
2023-11-01 13:39:25 -0700

    Update loretta file
    I don't think we can input a variable into the
    read_delim_chunked function, it requires a string
    that will take you to the delimited text file

--- elevatr_trial_loretta.R
+++ elevatr_trial_loretta.R
@@ -8,7 +8,7 @@ colnames(dat)[which(names(dat) == "geometry")] <-"latitude"
 colnames(dat)[which(names(dat) == "GEOID")] <-"longitude"
 
 dat$longitude<-gsub(")","",as.character(dat$longitude))
-dat$latitude<-gsub("c\(","",as.character(dat$latitude))
+dat$latitude<-gsub('c\\(',"",as.character(dat$latitude))
 
 
 crs_dd <- 4326
@@ -18,4 +18,6 @@ sf_dat <- sf::st_as_sf(dat, coords = c("longitude", "latitude"), crs = crs_dd)
 df_elev_epqs <- get_elev_point(sf_dat, prj = crs_dd, src = "aws", z=10)
 summary(df_elev_epqs)
 
+
+# The input into this function needs to be a file, we are inputting a variable so I don't think it'll work
 read_delim_chunked(df_elev_epqs)
